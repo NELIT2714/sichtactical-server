8644749883e99873827e802e50c3dca5cc4ae928
NELIT2714 <nelit2714@gmail.com>
2026-02-16 19:57:13 +0100

    feat: update CORS properties

--- src/main/java/dev/nelit/server/config/CorsProperties.java
+++ src/main/java/dev/nelit/server/config/CorsProperties.java
@@ -3,6 +3,7 @@ package dev.nelit.server.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Configuration
@@ -16,5 +17,7 @@ public class CorsProperties {
     public void setApiKey(String apiKey) { this.apiKey = apiKey; }
 
     public List<String> getAllowedOrigins() { return allowedOrigins; }
-    public void setAllowedOrigins(List<String> allowedOrigins) { this.allowedOrigins = allowedOrigins; }
+    public void setAllowedOrigins(String allowedOrigins) {
+        this.allowedOrigins = Arrays.asList(allowedOrigins.split(","));
+    }
 }
