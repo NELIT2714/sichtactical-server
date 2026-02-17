1193754eee82c76845b44a17ad9b262ba3de93f5
NELIT2714 <nelit2714@gmail.com>
2026-02-17 23:48:58 +0100

    feat: update sign up dto

--- src/main/java/dev/nelit/server/dto/event/EventSignUpDTO.java
+++ src/main/java/dev/nelit/server/dto/event/EventSignUpDTO.java
@@ -2,24 +2,35 @@ package dev.nelit.server.dto.event;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import dev.nelit.server.enums.EquipmentTypes;
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public class EventSignUpDTO {
 
+    @NotBlank
     @JsonProperty("full_name")
     private String fullName;
 
+    @NotBlank
+    @Pattern(regexp = "^\\+[0-9]{1,14}$")
     @JsonProperty("phone_number")
     private String phoneNumber;
 
+    @NotBlank
     @JsonProperty("call_sign")
     private String callSign;
 
+    @NotBlank
     @JsonProperty("equipment_type")
     private EquipmentTypes equipmentType;
 
+    @NotBlank
     @JsonProperty("save_data")
     private Boolean saveData;
 
+    @NotBlank
+    @AssertTrue
     @JsonProperty("accept_rules")
     private Boolean acceptRules;
 
